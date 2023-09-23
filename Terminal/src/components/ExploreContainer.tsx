c24756b9d18dba3e03cc93da2e51483d011152bb
Wayne <wguo6358@gmail.com>
2023-09-23 21:45:14 +0000

    debug canvas

--- Terminal/src/components/ExploreContainer.tsx
+++ Terminal/src/components/ExploreContainer.tsx
@@ -6,24 +6,28 @@ import { IonButton } from '@ionic/react';
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  var webcam: any;
+  const send = () => {
+    const picture = webcam.snap();
+    alert(picture)
+  }
   useEffect(() => {
     //fotgot this
     const webcamElement = document.getElementById('webcam');
     const canvasElement = document.getElementById('canvas');
     const snapSoundElement = document.getElementById('snapSound');
-    const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
+    webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
     webcam.start()
-    var picture = webcam.snap();
-    console.log(picture)
+
   }, [])
 
   return (
     <>
       {/* https://developer.mozilla.org/en-US/docs/Web/API/Media_Capture_and_Streams_API/Taking_still_photos */}
       <video id="webcam" autoPlay width={window.innerWidth}></video>
-      <canvas id="canvas" className="d-none"></canvas>
+      <canvas id="canvas" className="d-none" hidden></canvas>
       <audio id="snapSound" preload="auto"></audio>
-      <IonButton expand="block">Snap</IonButton>
+      <IonButton expand="block" onClick={send}>Snap</IonButton>
     </>
 
   );
