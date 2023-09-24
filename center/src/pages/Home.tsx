bb581591af1c889db049ba2ed408c1eddd6f861b
Wayne <wguo6358@gmail.com>
2023-09-24 07:45:54 +0000

    finished

--- center/src/pages/Home.tsx
+++ center/src/pages/Home.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { IonContent, IonGrid, IonRow, IonCol, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonList, IonTextarea } from '@ionic/react';
+import {React, useState} from 'react';
+import { IonContent, IonButton, IonGrid, IonRow, IonCol, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonList, IonTextarea } from '@ionic/react';
 import './Home.css';
 const address = 'https://8000-weathon-facebus-qiqi4k0uqz8.ws-us104.gitpod.io/'
 const Home: React.FC = () => {
+  const [res, setRes] = useState("")
   return (
     <IonPage>
       <IonHeader>
@@ -20,27 +21,37 @@ const Home: React.FC = () => {
         <IonGrid>
           <IonRow>
             <IonCol size="6">
-           
+
               <div className="left-half">
-               
-                 
-                  
-                    <img src={address+"/center_chart?id=1"} style={{width:'100%'}} />
-                  
-                  
-                
+
+                <img src={address + "/center_chart?id=1"} style={{ width: '100%' }} />
               </div>
             </IonCol>
             <IonCol size="6">
-              
+
               <div className="right-half">
-              <IonList>
-      <IonItem>
-        <IonTextarea label="Let GPT analyse data" placeholder="Type something here"></IonTextarea>
-      </IonItem>
-      
-       
-    </IonList>
+                <IonList>
+                  <IonItem>
+                    <IonTextarea label="Let GPT analyse data" placeholder="Type something here" id="text"></IonTextarea>
+                  </IonItem>
+
+                  <IonButton expand="block" onClick={() => {
+                    //@ts-ignore
+                    // console.log(document.getElementById("text").value)
+                    setRes("Loading...")
+                    fetch("https://8000-weathon-facebus-qiqi4k0uqz8.ws-us104.gitpod.io/chat?prompt=" + document.getElementById("text").value, { credentials: 'include' }).then(x => x.json())
+                      .then(data => {
+                        // alert(data)
+                        setRes(data)
+                      })
+                  }}>
+                    Submit
+                  </IonButton>
+                  <IonItem>
+                  <p>{res}</p>
+
+                  </IonItem>
+                </IonList>
               </div>
             </IonCol>
           </IonRow>
